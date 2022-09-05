52f79af9fea7dc3bfe6e66608ec8d8ffbafea624
João Luiz Pacher <80042677+jlpacher@users.noreply.github.com>
2022-09-05 11:14:37 -0300

    Update input.py
    Created function to filter data.
    Created range of mechanics.

--- input.py
+++ input.py
@@ -2,111 +2,124 @@ from promo_scheduling.entity import Mechanic, Partner, Promotion, SystemSettings
 import pandas as pd
 from google.cloud import bigquery
 
-client = bigquery.Client()
-project = "dotzcloud-datalabs-datascience"
-dataset_id = "DATA_SCIENCE_TEMP"
-
-dataset_ref = bigquery.DatasetReference(project, dataset_id)
-table_ref = dataset_ref.table("OTIMIZADOR_DADOS")
-table = client.get_table(table_ref)
-
-df = client.list_rows(table).to_dataframe()
-
-# Productivity
-partners_names=['AMAZON','Magazine Luiza.com','Americanas.com','Submarino']
-partners=['amzn','mglu','amer','subm']
-mechanics=['DZ_2','DZ_4','DZ_8']
-
-dic={}
-for p in partners:
-    for m in mechanics:
-        index=partners.index(p)
-        dic[p+'_'+m]=0 if pd.isna(df.loc[(df['NomeParceiro']==partners_names[index])&(df['PromoAjust'] == m)]['CliePorDia'].mean()) \
-                       else df.loc[(df['NomeParceiro'] == partners_names[index])&(df['PromoAjust'] == m)]['CliePorDia'].mean()
-
 system_settings = SystemSettings(min_duration=3)
 
-dz_2 = Mechanic('DZ2', 28)
-dz_4 = Mechanic('DZ4', 4)
-dz_8 = Mechanic('DZ8', 2)
+project = "dotzcloud-datalabs-datascience"
+dataset_id = "DATA_SCIENCE_TEMP"
+table_id = "OTIMIZADOR_DADOS"
 
-amzn = Partner('Amazon', 7)
-mglu = Partner('Magalu', 7)
-amer = Partner('Americanas', 7)
-subm = Partner('Submarino', 7)
+partners_names=['AMAZON','Magazine Luiza.com','Americanas.com','Submarino']
+# ['AMAZON','Americanas.com','Magazine Luiza.com','Netshoes','Casas Bahia','Submarino','Shop Time','RENNER','Ponto Frio','EXTRA.COM']
+partners_availability=[7,7,7,7]
+mechanics_names=[['DZ_2','DZ_3','DZ_4','DZ_5'],
+                 ['DZ_8','DZ_9','DZ_10','DZ_11','DZ_12'],
+                 ['DZ_14','DZ_16','DZ_17','DZ_18','DZ_19','DZ_20']]
+mechanics_availability=[28,4,4]
 
-amzn_jobs = [
+client = bigquery.Client()
+dataset_ref = bigquery.DatasetReference(project, dataset_id)
+table_ref = dataset_ref.table(table_id)
+table = client.get_table(table_ref)
+df = client.list_rows(table).to_dataframe()
+
+def productivity_base(df,partners_names,mechanics_names):
+    df_filter=df.loc[(df['NomeParceiro']==partners_names)&(df['PromoAjust'].isin(mechanics_names))]
+    result = df_filter['Clientes'].sum()/df_filter['DistinctDays'].sum()
+    return 0 if pd.isna(result) else result
+
+mechanic_a = Mechanic(mechanics_names[0],mechanics_availability[0])
+mechanic_b = Mechanic(mechanics_names[1],mechanics_availability[1])
+mechanic_c = Mechanic(mechanics_names[2],mechanics_availability[2])
+
+partner_a = Partner(partners_names[0], partners_availability[0])
+partner_b = Partner(partners_names[1], partners_availability[1])
+partner_c = Partner(partners_names[2], partners_availability[2])
+partner_d = Partner(partners_names[3], partners_availability[3])
+
+partner_a_jobs = [
     Promotion(
-        partner=amzn,
-        mechanic=dz_2,
-        productivity_ref=dic['amzn_DZ_2']
+        partner=partner_a,
+        mechanic=mechanic_a,
+        productivity_ref=productivity_base(df,
+            partners_names=partners_names[0],mechanics_names=mechanics_names[0])
     ),
     Promotion(
-        partner=amzn,
-        mechanic=dz_4,
-        productivity_ref=dic['amzn_DZ_4']
+        partner=partner_a,
+        mechanic=mechanic_b,
+        productivity_ref=productivity_base(df,
+            partners_names=partners_names[0],mechanics_names=mechanics_names[1])
     ),
     Promotion(
-        partner=amzn,
-        mechanic=dz_8,
-        productivity_ref=dic['amzn_DZ_8']
+        partner=partner_a,
+        mechanic=mechanic_c,
+        productivity_ref=productivity_base(df,
+            partners_names=partners_names[0],mechanics_names=mechanics_names[2])
     )
 ]
 
-mglu_jobs = [
+partner_b_jobs = [
     Promotion(
-        partner=mglu,
-        mechanic=dz_2,
-        productivity_ref=dic['mglu_DZ_2']
+        partner=partner_b,
+        mechanic=mechanic_a,
+        productivity_ref=productivity_base(df,
+            partners_names=partners_names[1],mechanics_names=mechanics_names[0])
     ),
     Promotion(
-        partner=mglu,
-        mechanic=dz_4,
-        productivity_ref=dic['mglu_DZ_4']
+        partner=partner_b,
+        mechanic=mechanic_b,
+        productivity_ref=productivity_base(df,
+            partners_names=partners_names[1],mechanics_names=mechanics_names[1])
     ),
     Promotion(
-        partner=mglu,
-        mechanic=dz_8,
-        productivity_ref=dic['mglu_DZ_8']
+        partner=partner_b,
+        mechanic=mechanic_c,
+        productivity_ref=productivity_base(df,
+            partners_names=partners_names[1],mechanics_names=mechanics_names[2])
     )
 ]
 
-amer_jobs = [
+partner_c_jobs = [
     Promotion(
-        partner=amer,
-        mechanic=dz_2,
-        productivity_ref=dic['amer_DZ_2']
+        partner=partner_c,
+        mechanic=mechanic_a,
+        productivity_ref=productivity_base(df,
+            partners_names=partners_names[2],mechanics_names=mechanics_names[0])
     ),
     Promotion(
-        partner=amer,
-        mechanic=dz_4,
-        productivity_ref=dic['amer_DZ_4']
+        partner=partner_c,
+        mechanic=mechanic_b,
+        productivity_ref=productivity_base(df,
+            partners_names=partners_names[2],mechanics_names=mechanics_names[1])
     ),
     Promotion(
-        partner=amer,
-        mechanic=dz_8,
-        productivity_ref=dic['amer_DZ_8']
+        partner=partner_c,
+        mechanic=mechanic_c,
+        productivity_ref=productivity_base(df,
+            partners_names=partners_names[2],mechanics_names=mechanics_names[2])
     )
 ]
 
-subm_jobs = [
+partner_d_jobs = [
     Promotion(
-        partner=subm,
-        mechanic=dz_2,
-        productivity_ref=dic['subm_DZ_2']
+        partner=partner_d,
+        mechanic=mechanic_a,
+        productivity_ref=productivity_base(df,
+            partners_names=partners_names[3],mechanics_names=mechanics_names[0])
     ),
     Promotion(
-        partner=subm,
-        mechanic=dz_4,
-        productivity_ref=dic['subm_DZ_4']
+        partner=partner_d,
+        mechanic=mechanic_b,
+        productivity_ref=productivity_base(df,
+            partners_names=partners_names[3],mechanics_names=mechanics_names[1])
     ),
     Promotion(
-        partner=subm,
-        mechanic=dz_8,
-        productivity_ref=dic['subm_DZ_8']
+        partner=partner_d,
+        mechanic=mechanic_c,
+        productivity_ref=productivity_base(df,
+            partners_names=partners_names[3],mechanics_names=mechanics_names[2])
     )
 ]
 
-possible_promotions = [*amzn_jobs, *mglu_jobs, *amer_jobs] #, *subm_jobs]
-partners = [amzn, mglu, amer]  # , subm]
-mechanics = [dz_2, dz_4, dz_8]
+possible_promotions = [*partner_a_jobs, *partner_b_jobs, *partner_c_jobs, *partner_d_jobs]
+partners = [partner_a, partner_b, partner_c, partner_d]
+mechanics = [mechanic_a, mechanic_b, mechanic_c]
